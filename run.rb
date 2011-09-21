afa72dd7195be3f9ecbb32cd1963b6df5eaf95e9
Andrew Ryan <nerdrew@gmail.com>
2011-09-21 17:35:09 -0400

    Update run.rb

--- run.rb
+++ run.rb
@@ -1,72 +1,95 @@
-require 'fileutils'
+require 'fileutils' 
 
-def sync
+TRACKING_PORTS = "tracking"
+MACPORTS_PORTS = "/opt/local/var/macports/sources/rsync.macports.org/release/tarballs/ports"
+#MACPORTS_PORTS = "/Users/andrew/CleanRoom/10.6opt/local/var/macports/sources/rsync.macports.org/release/ports"
+CUSTOM_PORTS = "custom"
+
+def repo_clean?(dir = nil)
   # Ensure the git repo is clean before starting
-  git_status = `git status --porcelain`
-  if /tracking/ =~ git_status || /custom/ =~ git_status
+  args = ['git', 'status', '--porcelain']
+  args << dir if dir
+  git_status = IO.popen(args){|io| io.read}
+  repo = dir ? /#{dir}/ : /#{TRACKING_PORTS}|#{CUSTOM_PORTS}/
+  if repo =~ git_status
     puts git_status
     puts "Commit changes before running!"
-    exit
+    return false
   end
+  return true
+end
 
-  tracking_ports = "tracking"
-  macports_ports = "/opt/local/var/macports/sources/rsync.macports.org/release/tarballs/ports"
-  #macports_ports = "/Users/andrew/CleanRoom/10.6opt/local/var/macports/sources/rsync.macports.org/release/ports"
-  custom_ports = "custom"
+def update_tracking_from_macports
+  exit if !repo_clean?
 
-  failed_diffs = []
-  Dir.foreach('custom') do |category|
+  Dir.foreach(CUSTOM_PORTS) do |category|
     next if category == "." || category == ".."
 
-    tracking = File.join(tracking_ports, category)
+    tracking = File.join(TRACKING_PORTS, category)
     FileUtils.mkdir_p(tracking) if !File.exists?(tracking)
-    Dir.foreach(File.join(custom_ports, category)) do |port|
+    Dir.foreach(File.join(CUSTOM_PORTS, category)) do |port|
       next if port == "." || port == ".."
 
-      macport = File.join(macports_ports, category, port)
+      macport = File.join(MACPORTS_PORTS, category, port)
       next if !File.exists?(macport)
 
       FileUtils.cp_r(macport, tracking)
+    end
+  end
+
+  if !repo_clean?(TRACKING_PORTS)
+    print "Commit tracking changes to git? You need to do this before continuing. (Y/n): "
+    if (STDIN.gets || 'y').chomp.downcase == 'y'
+      puts IO.popen(['git', 'add', TRACKING_PORTS]) {|io| io.read}
+      puts IO.popen(['git', 'commit', '-m', 'Auto-commit changes to tracking ports']) {|io| io.read} if $?.exitstatus == 0
+    end
+  end
+end
+
+def sync
+  exit if !repo_clean?
+
+  failed_diffs = []
+  Dir.foreach(CUSTOM_PORTS) do |category|
+    next if category == "." || category == ".."
+
+    tracking = File.join(TRACKING_PORTS, category)
+    Dir.foreach(File.join(CUSTOM_PORTS, category)) do |port|
+      next if port == "." || port == ".."
 
       puts port
-      diff = IO.popen(['git', 'diff', File.join(tracking, port)]) do |diff_io|
-        diff_io.read
-      end
-
+      diff = IO.popen(['git', 'diff', File.join(tracking, port)]) {|io| io.read}
       if diff.size > 0 && $?.exitstatus == 0
-        IO.popen(['git', 'apply', '-p2', "--directory=#{custom_ports}", '-'], 'w') do |apply_io|
-          apply_io.write(diff)
-        end
+        IO.popen(['git', 'apply', '-p2', "--directory=#{CUSTOM_PORTS}", '-'], 'w') {|io| io.write(diff)}
         if $?.exitstatus > 0
           File.open("#{category}_#{port}.diff", "w") {|f| f.write(diff)}
           failed_diffs << [category, port]
         end
       end
-
     end
-
   end
 
   puts "There were #{failed_diffs.size} failed patches."
   failed_diffs.each do |category, port|
-    puts "(e)dit, (s)kip, (a)bort: "
-    while input = gets
-      case input
-      when "e", "E"
-        output = IO.popen(['mvim', '-O', File.join("custom", category, port, "Portfile"), "#{category}_#{port}.diff"]) {|io| io.read}
-        puts output if output
-      when "s", "s"
-        next
-      else
-        break
-      end #case
-    end #while
+    print "#{category}/#{port} patch failed. (E)dit, (s)kip, (a)bort: "
+    case (STDIN.gets || 'e').chomp.downcase
+    when "e"
+      output = IO.popen(['mvim', '-O', File.join(CUSTOM_PORTS, category, port, "Portfile"), "#{category}_#{port}.diff"]) {|io| io.read}
+      puts output if output
+      print "Remove file '#{category}_#{port}.diff'? (Y/n): "
+      if (STDIN.gets || 'y').chomp.downcase == 'y'
+        FileUtils.rm("#{category}_#{port}.diff")
+      end
+    when "s"
+      next
+    else
+      break
+    end #case
   end #do
 
   # Copy the new updated custom ports to the macports sources folder
   #`sudo cp -r #{custom_ports}/* #{macports_ports}`
 
-  #puts `git add #{tracking_ports} && git commit -m "Auto-commit changes to tracking ports"`
   #puts `git add #{custom_ports} && git commit -m "Auto-commit tracking patches applied to custom ports"`
   #exit if $?.exitstatus > 0
 end #def
@@ -74,7 +97,7 @@ end #def
 def review_errors
 end
 
-def copy_from_macports(category, port)
+def copy_new_from_macports(category, port)
 end
 
 case ARGV[0]
